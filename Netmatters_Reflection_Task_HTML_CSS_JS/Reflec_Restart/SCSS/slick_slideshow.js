2711813e898e80bef6a6fc7e7fae91fa61f4c40b
Skye Breeze <skyexwarren@gmail.com>
2020-07-30 15:31:34 +0100

    Added Files

---
+++ Netmatters_Reflection_Task_HTML_CSS_JS/Reflec_Restart/SCSS/slick_slideshow.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+//     $('.image_slideshow').slick({
+//       setting-name: setting-value
+//     });
+//   });
+
+
+$('.image_slideshow').slick({
+  autoplay: true,
+  autplaySpeed: 4000,
+  dots: true
+})
+

e59f739e04261c579e39f62cf267c4e18e7a1bc1
Skye Breeze <skyexwarren@gmail.com>
2020-09-30 17:13:15 +0100

    Side Menu JS Code and Styles

--- HTML_CSS_JS_Reflection_Finished/Reflec_Restart/JS/cookie_modal.js
+++ HTML_CSS_JS_Reflection_Finished/Reflec_Restart/JS/cookie_modal.js
@@ -1,11 +1,11 @@
 const modalObject = document.querySelector("#modal")
 const modalSettings = document.querySelector(".settings_button")
 const modalAccept = document.querySelector('.modal_accept_button')
-const modalOverlay = document.querySelector("#overlay")
+const modalOverlay = document.getElementById("modal_overlay")
 
 // localStorage.setItem('cookie', 'false')
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', function() {
     if (localStorage.getItem('cookie') == 'true') {
         console.log("Cookie has already been accepted.")
     } else if (localStorage.getItem('cookie') == null || 
@@ -20,13 +20,19 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 
 function showModal() {
-    modalObject.classList.remove('deactive')
-    modalOverlay.classList.remove('deactive')
+    // modalObject.classList.remove('deactive')
+    // modalOverlay.classList.remove('deactive')
+
+    modalOverlay.classList.add('active')
+    modalObject.classList.add('active')
 }
 
 function disableModal() {
-    modalObject.classList.add('deactive')
-    modalOverlay.classList.add('deactive')
+    // modalObject.classList.add('deactive')
+    // modalOverlay.classList.add('deactive')
+
+    modalOverlay.classList.remove('active')
+    modalObject.classList.remove('active')
 }
 
 
---
+++ HTML_CSS_JS_Reflection_Finished/Reflec_Restart/JS/side_menu.js
@@ -0,0 +1,42 @@
+
+
+let burgerLines = document.getElementsByClassName('burger_line')
+const burgerButton = document.getElementById('burger_button')
+const side_menu = document.getElementById('side_menu')
+const menu_overlay = document.getElementById('menu_overlay')
+
+let burgerState = false
+
+burgerButton.addEventListener('click', function() {
+    console.log('Button Clicked')
+
+    if (burgerState) {
+        SideMenuOff()
+    } else {
+        SideMenuOn()
+    }
+})
+
+menu_overlay.addEventListener('click', function() {
+    SideMenuOff()
+})
+
+function SideMenuOn() {
+    burgerLines[1].style.opacity = "0"
+    burgerLines[0].classList.add('tilt_left')
+    burgerLines[2].classList.add('tilt_right')
+    side_menu.classList.add('side_menu_transform')
+    menu_overlay.classList.add('active')
+    burgerState = true
+    window.removeEventListener('scroll', noScroll)
+}
+
+function SideMenuOff() {
+    burgerLines[1].style.opacity = "1"
+    burgerLines[0].classList.remove('tilt_left')
+    burgerLines[2].classList.remove('tilt_right')
+    side_menu.classList.remove('side_menu_transform')
+    menu_overlay.classList.remove('active')
+    burgerState = false
+    window.addEventListener('scroll', noScroll)
+}

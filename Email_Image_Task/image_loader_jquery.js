33aece103424d4309364a696ff0c25536181ba6e
Skye Breeze <skyexwarren@gmail.com>
2020-12-21 18:44:03 +0000

    End of Day Commit

--- Email_Image_Task/image_loader_jquery.js
+++ Email_Image_Task/image_loader_jquery.js
@@ -4,9 +4,10 @@ $placeholder_image = $('#placeholder')
 $add_button = $('#add_button')
 $new_image_button = $('#new_button')
 $email_input = $('#email_input')
-$email = null
+var inputEmail
+var email = null
 var email_array = []
-var imageURL
+$imageURL = null
 
 $( document ).ready(function() {
     previewImage()
@@ -22,19 +23,30 @@ function previewImage() {
     } while (imageID == id)
 
     $.getJSON('https://picsum.photos/id/' + id + '/info', function(data) {
-        imageURL = 'https://picsum.photos/id/' + data.id + '/500/500'
-        $preview_image.attr('src', imageURL)
+        $imageURL = 'https://picsum.photos/id/' + data.id + '/500/500'
+        $preview_image.attr('src', $imageURL)
         $placeholder_image.attr('class', 'placeholder preview_image hide')
         imageID = id
     })
 }
 
 function validateEmail() {
+
+
     var _email = $email_input.val()
     var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/
 
     if (regex.test(_email)) {
-        $email = $email_input.val()
+        var uncutEmail = String(_email)
+        var cutEmail = ''
+        inputEmail = _email
+        for (var x = 0; x < uncutEmail.length; x++) {
+            if (uncutEmail.charAt(x) != '@' || uncutEmail.charAt(x) != '.') {
+                cutEmail += uncutEmail.charAt(x)
+            }
+        }
+
+        email = String(cutEmail)
         return true
     } else {
         return false
@@ -49,19 +61,26 @@ $add_button.click(function() {
 
         // email has passed validation
         $email_image_container = $('#email_image_container')
-        console.log($email_image_container)
 
         // does the email element already exist
+        // if (jQuery.inArray('hello', email_array) >= 0) {
+        if (jQuery.inArray(email, email_array) >= 0) {
+            var id = '#' + email
+            $additionalImageElement = `<img src="${$imageURL}">`
+            $imageContainer = $('#skyexwarrengmailcom')
+            // console.log($imageContainer)
+            $imageContainer.append($additionalImageElement)
 
-        if (jQuery.inArray($email, email_array) >= 0) {
-            $image_container = $('#' + $email)
-            $image_container.append('<img src="' + imageURL + '">')
         } else {
-            console.log($email)
-            var imageContainer = '<div class="image_container id="' +$email + '><img src="' + imageURL + '"></div>' 
-            var emailContainer = '<div class="email_container"><p>'+ $email +'</p>' + imageContainer + '</div>'
-            $email_image_container.append(emailContainer)
-            email_array.push($email)
+
+            $imageElement = `<img src="${$imageURL}">`
+            // $imageContainer = `<div class="image_container" id="hello">${$imageElement}</div>` 
+            $imageContainer = '<div class="image_container" id="skyexwarrengmailcom">' + $imageElement + '</div>'
+            $emailContainer = '<div class="email_container"><p>' + inputEmail + '</p>' + $imageContainer + '</div>'
+            $email_image_container.prepend($emailContainer)
+            email_array.push(email)
+            // email_array.push('hello')
+
         }
 
         previewImage()
